3a5c22920cb40507ff0e48b5402cf99df1dad0ec
chris <chrishethertonweb@gmail.com>
2022-09-05 13:22:31 +0200

    added constructor

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
-class Calculator{
-    constructor(){
+class Calculator {
+    constructor(x, y) {
+
+        this.x = x
+        this.y = y
+
+        this.pi = 3.141592653589793
+        this.e = 2.718281828459045
         //properties
     }
 
+
     //getter
-    
+
     //methods
 }
