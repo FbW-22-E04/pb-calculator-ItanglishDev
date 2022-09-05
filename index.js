fe86035e611583b702d8b32be1cfc91bff47183a
chris <chrishethertonweb@gmail.com>
2022-09-05 15:19:14 +0200

    added subtract, multiply and divide method

--- index.js
+++ index.js
@@ -26,7 +26,20 @@ class Calculator {
         return this.x + this.y
     }
 
+    subtract() {
 
+        return this.x - this.y
+    }
+
+    multiply() {
+
+        return this.x * this.y
+    }
+
+    divide() {
+
+        return this.x / this.y
+    }
 
 
     //getter
